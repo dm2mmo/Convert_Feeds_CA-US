9005fbe5d2a6cabcf24a6e087a0178514708ced7
dm2mmo <dm2.mmo@gmail.com>
2025-08-14 13:56:48 +0700

    Update blacklist, change Rubiks to Cube

--- Convert_CA_to_US_Feeds_OK.py
+++ Convert_CA_to_US_Feeds_OK.py
@@ -50,7 +50,7 @@ def convert_amazon_feed():
     # --- Blacklist Keywords (DANH SÁCH BLACKLIST MỚI) ---
     blacklist_keywords = [
         "Whyitsme", "Cottagecore", "Trump", "Biden", "Reggae", "Smoke Daddy", "Celtic Cross", "Bob Marley", "Family Guy", "Gay Cat", "Gay Trash", "Jockey", "Fishy", "Venom", "Boba", "BSN", "Uterus", "Van Gogh",
-        "CARHARTT", "Nonni", "Kangaroo", "Tuxedo", "Dibble", "Dabble", "Oh ship", "comica", "COHIBA", "Jurassic", "Jeep", "Jeeps", "Rubiks", "Adventure Before Dementia", "antisocial", "anti social", "Cobra", "Python",
+        "CARHARTT", "Nonni", "Kangaroo", "Tuxedo", "Dibble", "Dabble", "Oh ship", "comica", "COHIBA", "Jurassic", "Jeep", "Jeeps", "Adventure Before Dementia", "antisocial", "anti social", "Cobra", "Python",
         "Spirit Halloween", "Got Titties", "Le Tits Now", "Mack Trucks", "V-buck", "V buck", "Vbuck", "World Traveler", "Rollerblade", "Black Lives Matter", "Just The Tip", "In My Defense", "Van Gogh",
         "U.S.Army", "US Army", "Crazy Chicken Lady", "Christmas In July", "Grill Sergeant", "Ducks Unlimited", "SOTALLY Tober", "Birds aren't Real", "Pickleballer", "Quaker", "Vampire Mansion",
         "Lampoon's", "Lampoons", "Lampoon", "krampus", "griswold", "Brainrot", "Disney", "Marvel", "Star Wars", "Music Television", "MTV", "Fender", "Nightmare Before Christmas", "Life is Good",
@@ -62,6 +62,7 @@ def convert_amazon_feed():
     # --- Keyword Replacements (DANH SÁCH THAY THẾ TỪ KHÓA MỚI) ---
     keyword_replacements = {
         "Guess": "Funny",
+        "Rubiks": "Cube",
         "Sakura": "Flower",
         "Superhero": "Heroes",
         "Yeti": "Bigfoot",
